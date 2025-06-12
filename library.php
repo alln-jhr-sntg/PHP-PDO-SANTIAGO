16b76a201eccf256d387be167b90ad557bb9cf1c
allenjherusantiago <allenjherussantiago@gmail.com>
2025-06-12 20:54:15 +0800

    Library Book Management
    ---Connecting MySQL database using PDO.
    ---INSERT query for adding new books.
    ---SELECT query for viewing all books in the database currently.
    ---UPDATE query for updating information on any books.
    ---DELETE query for deleting books in the database.

---
+++ library.php
@@ -0,0 +1,79 @@
+<?php 
+    // connect to the database
+    $host="localhost";
+    $user="root";
+    $password="";
+    $dbname="library_db";
+
+    //data source name
+    $dsn="mysql:host=$host;dbname=$dbname";
+    
+    $conn = new PDO($dsn, $user, $password);
+
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    /*INSERT query
+    $title = "Scout's Guide to the Galaxy";
+    $author = "Douglas Adams";
+    $genre = "Science Fiction";
+    $published_year = "1979";
+
+    $sql = "INSERT INTO books (title, author, genre, published_year) VALUES (:titlez, :authorz, :genrez, :published_yearz)";
+    $statement = $conn->prepare($sql);
+
+    $statement->execute([
+        ':titlez' => $title,
+        ':authorz' => $author,
+        ':genrez' => $genre,
+        ':published_yearz' => $published_year
+    ]);
+    // Check if the book was added successfully
+    if ($statement->rowCount() > 0) {
+        echo "Book added successfully!";
+    } else {
+        echo "Failed to add book.";
+    }
+    */
+
+    /* SELECT query
+    $statement = $conn->query("SELECT * FROM books");
+    $books = $statement->fetchAll(PDO::FETCH_OBJ);
+    foreach($books as $book) {
+        echo "Title: " . $book->title . "<br />";
+        echo "Author: " . $book->author . "<br />";
+        echo "Genre: " . $book->genre . "<br />";
+        echo "Published Year: " . $book->published_year . "<br /><br />";
+    }
+    */
+
+    /* UPDATE query
+    $author = "Douglas Adams";
+    $new_title = "The Hitchhiker's Guide to the Galaxy";
+
+    $sql = "UPDATE books SET title = :titlez WHERE author = :authorz";
+    $statement = $conn->prepare($sql);
+    $statement->execute([
+        ':titlez' => $new_title,
+        ':authorz' => $author
+    ]);
+    // Check if the book was updated successfully
+    if ($statement->rowCount() > 0) {
+        echo "Book updated successfully!";
+    } else {
+        echo "Failed to update book.";
+    }
+    */
+
+    /* DELETE query
+    $id_delete = 4;
+    $sql = "DELETE FROM books WHERE id = :idz";
+    $statement = $conn->prepare($sql);
+    $statement->execute([':idz' => $id_delete]);
+    // Check if the book was deleted successfully
+    if ($statement->rowCount() > 0) {
+        echo "Book deleted successfully!";
+    } else {
+        echo "Failed to delete book.";
+    }
+    */
+?>
